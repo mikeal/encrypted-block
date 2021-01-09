e51e69639ea5a0efa482c52cfed2b2e2700d47f9
Mikeal Rogers <mikeal.rogers@gmail.com>
2021-01-09 05:50:22 +0000

    init: initial implementation

---
+++ test/test-basics.js
@@ -0,0 +1,27 @@
+/* globals describe, it */
+import{ encode, decode, crypto, name, code } from '../index.js'
+import { sha256 as hasher } from 'multiformats/hashes/sha2'
+import raw from 'multiformats/codecs/raw'
+import { codec } from 'multiformats/codecs/codec'
+import { encode as encodeBlock, decode as decodeBlock } from 'multiformats/block'
+import { randomBytes } from 'crypto'
+import * as Block from 'multiformats/block'
+import { deepStrictEqual as same } from 'assert'
+
+const eb = codec({ encode, decode, name, code })
+
+describe('encrypted-block', () => {
+  it('encrypt/decrypt raw block', async () => {
+    const { encrypt, decrypt } = crypto(randomBytes(32))
+    const value = Buffer.allocUnsafe(128).fill('x')
+    const block = await Block.encode({ value, hasher, codec: raw })
+    const { value: encrypted } = await encrypt(block)
+    const eBlock = await Block.encode({ value: encrypted, codec: eb, hasher })
+    same(Buffer.compare(value, eBlock.bytes) !== 0, true)
+
+    const { value: val } = await Block.decode({ ...eBlock, codec: eb, hasher })
+    const { cid, bytes } = await decrypt({ value: val })
+    const dBlock = await Block.decode({ cid, bytes, codec: raw, hasher })
+    same(Buffer.compare(dBlock.value, value), 0)
+  })
+})
